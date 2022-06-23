a6095e3a7edca31417f11766b3c30eccc8e490ac
Viktor Hiscock <viktor.hiscock@c02zqhm1md6m.ocado.com>
2022-06-23 23:09:19 +0100

    Create Main class

---
+++ src/main/java/com/imgarena/licensing/tennis/TennisMatchLicensing.java
@@ -0,0 +1,7 @@
+package com.imgarena.licensing.tennis;
+
+public class TennisMatchLicensing {
+    public static void main(String[] args) {
+        System.out.println("tennis-match-licensing application is running");
+    }
+}
