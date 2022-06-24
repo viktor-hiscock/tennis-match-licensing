8c7fcb2be0f338c91093e870f2d3bd8665a857fe
Viktor Hiscock <viktor.hiscock@c02zqhm1md6m.ocado.com>
2022-06-24 15:22:55 +0100

    Add constants for table names

---
+++ src/main/java/com/imgarena/licensing/tennis/model/TableNames.java
@@ -0,0 +1,7 @@
+package com.imgarena.licensing.tennis.model;
+
+public class TableNames {
+    public static final String TENNIS_MATCH_LICENSE = "TENNIS_MATCH_LICENSE";
+    public static final String TENNIS_MATCH = "TENNIS_MATCH";
+    public static final String TENNIS_PLAYER = "TENNIS_PLAYER";
+}
