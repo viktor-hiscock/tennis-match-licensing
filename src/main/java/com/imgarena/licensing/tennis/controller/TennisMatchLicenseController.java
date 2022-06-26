187b13ac2ba58fd8de69a6ed75ecfc0af2785bbe
Viktor Hiscock <viktor.hiscock@c02zqhm1md6m.ocado.com>
2022-06-26 10:10:17 +0100

    Add endpoints to get all and delete tennis match license

--- src/main/java/com/imgarena/licensing/tennis/controller/TennisMatchLicenseController.java
+++ src/main/java/com/imgarena/licensing/tennis/controller/TennisMatchLicenseController.java
@@ -8,11 +8,16 @@ import com.imgarena.licensing.tennis.service.TennisMatchLicenseService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @AllArgsConstructor
@@ -31,5 +36,24 @@ public class TennisMatchLicenseController {
         TennisMatchLicense newTennisMatchLicense = tennisMatchLicenseService.createTennisMatchLicense(createTennisMatchLicenseDTO);
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(TennisMatchLicenseMapper.convertToTennisMatchLicenseResponseDTO(newTennisMatchLicense));
+    }
+
+    @DeleteMapping("v1/license/tennis/match/{tennisMatchLicenseId}")
+    public ResponseEntity<TennisMatchLicenseResponseDTO> deleteTennisMatchLicense(@PathVariable("tennisMatchLicenseId") Long tennisMatchLicenseId) {
+        TennisMatchLicense tennisMatchLicense = tennisMatchLicenseService.deleteTennisMatchLicense(tennisMatchLicenseId);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(TennisMatchLicenseMapper.convertToTennisMatchLicenseResponseDTO(tennisMatchLicense));
+    }
+
+    @GetMapping("v1/license/tennis/match")
+    public ResponseEntity<List<TennisMatchLicenseResponseDTO>> getAllTennisMatchLicenses(
+            @RequestParam(defaultValue = "0") int pageNumber,
+            @RequestParam(defaultValue = "10") int pageSize,
+            @RequestParam(defaultValue = "id") String sortBy
+    ) {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(tennisMatchLicenseService.getAllTennisMatchLicenses(pageNumber, pageSize, sortBy).stream()
+                        .map(TennisMatchLicenseMapper::convertToTennisMatchLicenseResponseDTO)
+                        .collect(Collectors.toList()));
     }
 }
--- src/main/java/com/imgarena/licensing/tennis/service/TennisMatchLicenseService.java
+++ src/main/java/com/imgarena/licensing/tennis/service/TennisMatchLicenseService.java
@@ -7,7 +7,15 @@ import com.imgarena.licensing.tennis.model.TennisMatch;
 import com.imgarena.licensing.tennis.model.TennisMatchLicense;
 import com.imgarena.licensing.tennis.repository.TennisMatchLicenseRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
@@ -29,5 +37,26 @@ public class TennisMatchLicenseService {
                 .tennisMatch(tennisMatch)
                 .build();
         return tennisMatchLicenseRepository.save(newTennisMatchLicense);
+    }
+
+    public TennisMatchLicense deleteTennisMatchLicense(Long tennisMatchLicenseId) {
+        Optional<TennisMatchLicense> tennisMatchLicenseToDelete = tennisMatchLicenseRepository.findById(tennisMatchLicenseId);
+        if (tennisMatchLicenseToDelete.isPresent()) {
+            tennisMatchLicenseRepository.delete(tennisMatchLicenseToDelete.get());
+        } else {
+            throw new TennisMatchLicenseNotFoundException(tennisMatchLicenseId);
+        }
+        return tennisMatchLicenseToDelete.get();
+    }
+
+    public List<TennisMatchLicense> getAllTennisMatchLicenses(int pageNumber, int pageSize, String sortBy) {
+        Pageable paging = PageRequest.of(pageNumber, pageSize, Sort.by(sortBy));
+        Page<TennisMatchLicense> pagedResult = tennisMatchLicenseRepository.findAll(paging);
+
+        if(pagedResult.hasContent()) {
+            return pagedResult.getContent();
+        } else {
+            return new ArrayList<>();
+        }
     }
 }
